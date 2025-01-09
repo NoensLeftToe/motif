1f4ce46ac03909057329cd4a6de5aaed7c08b93c
Sweta <73096700+nazinegi@users.noreply.github.com>
2025-01-09 12:55:22 +0530

    register and login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,32 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+
+const generateAccessAndRefreshTokens = async(userId) =>
+{
+    try {
+       const user = await User.findById(userId)
+       const accessToken = user.generateAccessToken()
+       const refreshToken = user.generateRefreshToken()
+
+       user.refreshToken = refreshToken
+       user.save({validateBeforeSave: false})
+       
+       return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, " Something went wrong while generation refresh and access token")
+    }
+}
+
+
+
+
+
+
 const registerUser = asyncHandler( async(req,res)=>{
    //get user details from frontend 
    //validation user like if the user is using correct gmail etc
@@ -16,7 +42,7 @@ const registerUser = asyncHandler( async(req,res)=>{
    //return response or send the error 
 
   const {fullname, email, username, password} = req.body
-   console.log("email: ", email);
+//    console.log("email: ", email);
 
    if(
     [fullname, email, username, password].some((field)=>
@@ -76,4 +102,92 @@ const existedUser = await User.findOne({
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req, res)=>{
+//req body => bring data 
+//username/email based access
+//find the user
+//password check 
+//access and refresh token and send both to the user
+//send cookies
+
+const {email, username, password} = req.body
+console.log(email);
+
+if(!username && !email){
+    throw new ApiError(400, " username or email is required")
+}
+
+const user =  await User.findOne({
+    $or : [{username}, {email}] // using or operator to find either username or email
+})
+
+if(!user){
+    throw new ApiError(404, "User does not exist")
+}
+
+const isPasswordValid = await user.isPasswordCorrect(password)
+
+if(!isPasswordValid){
+    throw new ApiError(401, "Credentials Invalid")
+}
+
+
+
+
+const{accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+const options = {
+    httpOnly: true,
+    secure: true
+}
+
+return res.status(200)
+.cookie("accessToken", accessToken, options)
+.cookie("refreshToken", refreshToken, options)
+.json(
+    new ApiResponse(
+        200,{
+            user: loggedInUser, accessToken,
+            refreshToken
+        },
+        "user logged in successfully"
+    )
+)
+
+
+})
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
+    
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+
+}
